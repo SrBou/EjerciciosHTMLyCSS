5d23a939f6bd1b013bffb13e174f9ee8c7e4f6b9
Sergio <sergiobouzasantas@gmail.com>
2023-07-17 14:41:52 +0200

    New commit

--- JS/ej05/script.js
+++ JS/ej05/script.js
@@ -1,10 +1,29 @@
+function alarm (){
+    cont = document.querySelector("#cont").value;    
+    alarmID = window.setTimeout(() => {                     
+        window.alert("La alarma está sonando!!");
+    }, cont * 1000);
+}
+
+function posponer () {
+    cont = cont + document.querySelector("#cont").value;
+    clearTimeout(alarmID);
+    alarmID = window.setTimeout(() => {                     
+        window.alert("La alarma está sonando!!");
+    }, cont * 1000);
+}
+
+function stop () {
+    clearTimeout(alarmID);
+}
+
 window.onload = () => {     
-    const btn = document.querySelector("#btn");
-    btn.addEventListener("click", () => {
-        const cont = document.querySelector("#cont").innerHTML;    
-        console.log(cont);
-        window.setTimeout(() => {                     
-            window.alert("La alarma está sonando!!")
-        }, cont * 1000);
-    }); 
+    const btnInicio = document.querySelector("#btnInicio");   
+    const btnPosponer = document.querySelector("#btnPosponer");   
+    const btnStop = document.querySelector("#btnStop");
+    let cont = 0;   
+    let alarmID = 0;
+    btnInicio.addEventListener("click", alarm); 
+    btnPosponer.addEventListener("click", posponer);
+    btnStop.addEventListener("click", stop);
 }   
